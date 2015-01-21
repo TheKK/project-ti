4022680883aed866d33dbee6b5b358a574c1971e
TheKK <thumbd03803@gmail.com>
2015-01-21 21:12:32 +0800

    Add class AnimatedSprite

---
+++ animatedSprite.cpp
@@ -0,0 +1,57 @@
+#include "animatedSprite.h"
+
+#include <SDL2/SDL_image.h>
+#include <stdexcept>
+
+#include "graphics.h"
+
+AnimatedSprite::AnimatedSprite(Graphics& graphics, const std::string& filePath,
+			       const SDL_Rect& startClipRect, int frameNum,
+			       int period):
+	spriteSheet_(graphics.loadSprite(filePath)),
+	startClipRect_(startClipRect),
+	drawClipRect_(startClipRect),
+	totoalFrameNum_(frameNum),
+	currentFrame_(0),
+	periodMax_(period),
+	periodCounter_(0)
+{
+}
+
+AnimatedSprite::~AnimatedSprite()
+{
+}
+
+void
+AnimatedSprite::load(Graphics& graphics, const std::string& filePath,
+		     const SDL_Rect& startClipRect, int frameNum, int period)
+{
+	spriteSheet_ = graphics.loadSprite(filePath);
+
+	startClipRect_ = startClipRect;
+	drawClipRect_ = startClipRect;
+
+	periodMax_ = period;
+	periodCounter_ = 0;
+}
+void
+AnimatedSprite::update()
+{
+	++periodCounter_;
+	if (periodCounter_ == periodMax_) {
+		periodCounter_ = 0;
+
+		++currentFrame_;
+		if (currentFrame_ == totoalFrameNum_)
+			currentFrame_ = 0;
+
+		drawClipRect_.x = (startClipRect_.x +
+				   startClipRect_.w * currentFrame_);
+	}
+}
+
+void
+AnimatedSprite::render(const Graphics& graphics, SDL_Rect& dstRect)
+{
+	graphics.render(spriteSheet_.get(), drawClipRect_, dstRect);
+}
---
+++ animatedSprite.h
@@ -0,0 +1,33 @@
+#ifndef ANIMATED_SPRITE_H
+#define ANIMATED_SPRITE_H
+
+#include <SDL2/SDL.h>
+#include <string>
+#include <memory>
+
+class Graphics;
+
+class AnimatedSprite
+{
+public:
+	AnimatedSprite(Graphics& graphics, const std::string& filePath,
+		       const SDL_Rect& startClipRect, int frameNum, int period);
+	~AnimatedSprite();
+
+	void load(Graphics& graphics, const std::string& filePath,
+		  const SDL_Rect& startClipRect, int frameNum, int period);
+
+	void update();
+	void render(const Graphics& graphics, SDL_Rect& dstRect);
+private:
+	std::shared_ptr<SDL_Texture> spriteSheet_;
+	SDL_Rect startClipRect_;
+	SDL_Rect drawClipRect_;
+
+	const int totoalFrameNum_;
+	int currentFrame_;
+
+	int periodMax_;
+	int periodCounter_;
+};
+#endif /* ANIMATED_SPRITE_H */
